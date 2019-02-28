19e165271a7c1ac90a8331195cd41785876a65c7
Dmytry Koren <altneox@gmail.com>
2019-02-28 10:53:31 +0200

    prettify views a bit, update specs to use css selectors

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,10 +1,10 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "Home page", type: :feature do
-  scenario "User visits home page" do
-    visit "/"
+RSpec.feature 'Home page', type: :feature do
+  scenario 'User visits home page' do
+    visit '/'
 
-    expect(page).to have_text("Welcome to url shortener!")
-    expect(page).to have_css("#submit-url-form")
+    expect(page).to have_css('.home-welcome-msg')
+    expect(page).to have_css('#submit-url-form')
   end
 end
