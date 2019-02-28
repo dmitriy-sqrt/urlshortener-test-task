1f61527dd622d450ea70628638d6caff90e2bd5b
Dmytry Koren <altneox@gmail.com>
2019-02-28 18:21:20 +0200

    show raw stats

--- app/controllers/dashboard/stats_controller.rb
+++ app/controllers/dashboard/stats_controller.rb
@@ -1,14 +1,13 @@
 module Dashboard
   class StatsController < ApplicationController
     def show
-
+      load_link
     end
-
 
     private
 
     def load_link
-      @link = Link.find_by!(uuid: params[:uuid])
+      @link = Link.find_by!(secret: params[:secret])
     end
   end
 end
--- spec/features/url_secret_key_spec.rb
+++ spec/features/url_secret_key_spec.rb
@@ -1,23 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'Secret links', type: :feature do
-  scenario 'getting secret link' do
-    visit '/'
-    fill_in 'link[full_url]', with: 'https://www.youtube.com/watch?v=uUtymxox_G8'
-    click_button 'Get url!' #TODO: css
-
-    expect(page).to have_css('.secret-stats-link a')
-  end
-  # TODO : DRY
   describe 'accessing secret link' do
-    it 'returns link stats' do
-
+    it 'shows link stats' do
       visit '/'
       fill_in 'link[full_url]', with: 'https://www.youtube.com/watch?v=uUtymxox_G8'
       click_button 'Get url!' #TODO: css
-
-      expect(page).to have_css('.secret-stats-link a')
-
       find('.secret-stats-link a').click
 
       expect(page).to have_css('#url-stats')
